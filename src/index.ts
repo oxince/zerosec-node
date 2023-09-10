1693534a3f5255025817f6a07a7558a61afb4dc9
oxince <scxrfx@gmail.com>
2023-09-10 03:56:27 +0200

    Added more of the api features.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
-import { ObfuscationPayload, ObfuscationResult, ObfuscationNode } from './interfaces';
+import { ObfuscationPayload, ObfuscationResult, ObfuscationNode, ApiKeyInfo, Obfuscation } from './interfaces';
 
 export default class ZeroSecAPI {
   private api: AxiosInstance;
+  private apiKey: string;
 
   constructor(apiKey: string) {
     if (!apiKey)
       throw new Error('API key is required');
 
+    this.apiKey = apiKey;
     this.api = axios.create({
       baseURL: 'https://api.zero.sex/v1',
       headers: {
@@ -27,10 +29,36 @@ export default class ZeroSecAPI {
 
     return data;
   }
+
+  async getApiKeyInfo(): Promise<ApiKeyInfo> {
+    const {
+      data,
+      status
+    } = await this.api.get(`/key/${this.apiKey}/info`);
+
+    if (status !== 200)
+      throw new Error(data.error);
+
+    return data;
+  }
+
+  async getObfuscations(): Promise<Obfuscation[]> {
+    const {
+      data,
+      status
+    } = await this.api.get(`/key/${this.apiKey}/obfuscations`);
+
+    if (status !== 200)
+      throw new Error(data.error);
+
+    return data;
+  }
 }
 
 export {
   ObfuscationNode,
   ObfuscationResult,
-  ObfuscationPayload
+  ObfuscationPayload,
+  ApiKeyInfo,
+  Obfuscation
 };
