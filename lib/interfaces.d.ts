3ef0429abc8953032514ce6a8c0fbd41868a9b18
oxince <scxrfx@gmail.com>
2023-10-08 22:56:41 +0200

    Forgot build with last commit

--- lib/interfaces.d.ts
+++ lib/interfaces.d.ts
@@ -34,6 +34,8 @@ interface ObfuscationPayload {
     script: string;
     /** The platform for which the script will be obfuscated. Supported: lua, roblox, csgo, fivem. */
     platformLock?: "lua" | "roblox" | "csgo" | "fivem";
+    /** The watermark for the obfuscated file */
+    watermark?: string;
     /** Enables prevention of tampering with the script. Requires support for `loadstring` or `load` in the environment. */
     antiTamper?: boolean;
     /** Enables string encryption in the script (may impact performance, not recommended). */
@@ -45,4 +47,46 @@ interface ObfuscationPayload {
     /** Whether to include a URL in the response or not. */
     giveBackURL?: boolean;
 }
-export { ObfuscationNode, ObfuscationResult, ObfuscationPayload };
+/**
+ * Represents the result of an api-key info request.
+ */
+interface ApiKeyInfo {
+    /** The ID of the API key. */
+    id: number;
+    /** The API key. */
+    apiKey: string;
+    /** The author of the API key. */
+    author: string;
+    /** The amount of authentications made with the API key. */
+    authentications: number;
+    /** Whether the API key is enabled or not. This can be changed by admins. */
+    enabled: boolean;
+    /** The date at which the API key expires. */
+    expiresAt: string;
+    /** The date at which the API key was created. */
+    createdAt: string;
+    /** The date at which the API key was last updated. */
+    updatedAt: string;
+    /** Whether the API key is active or not based on the expiration date. */
+    active: boolean;
+}
+/**
+ * Represents the result of an obfuscation request.
+ */
+interface Obfuscation {
+    /** The ID of the obfuscation. */
+    id: number;
+    /** The fingerprint of the obfuscation. */
+    fingerprint: string;
+    /** The API key used for the obfuscation. */
+    apiKey: string;
+    /** The size of the obfuscated file. */
+    fileSize: number;
+    /** The IP address of the server that requested the obfuscation. */
+    ip: string;
+    /** Whether the obfuscation was successful or not. */
+    success: boolean;
+    /** The date at which the obfuscation was requested. */
+    createdAt: string;
+}
+export { ObfuscationNode, ObfuscationResult, ObfuscationPayload, ApiKeyInfo, Obfuscation };
